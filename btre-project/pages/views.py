ab9fc1c10dca55e735dc5b4e99e0f83a89ef4612
amirzp/btre-projecct <amirzp>
2021-02-28 23:21:44 +0330

    complete about&index jinja code

--- btre-project/pages/views.py
+++ btre-project/pages/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from listings.models import Listings
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    lisitngs = Listings.objects.order_by(
+        '-is_data'
+        ).filter(is_published=True)[:3]
+
+    context = {
+        'listings': lisitngs
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_data').all()
+    mvp_realtors = Realtor.objects.order_by(
+        '-hire_data'
+        ).filter(is_mvp=True)[:1]
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', context)
